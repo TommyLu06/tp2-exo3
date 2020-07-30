1272de7a88cdc6c98820b95d97f7da97972b4f37
Tommy <67933350+TommyLu06@users.noreply.github.com>
2020-07-30 14:14:17 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,29 @@
-let x = randint(0, 4)
-let y = 0
-let y1 = -1
-let y2 = -2
+function deplace_led (colonne: number) {
+    for (let ligne = 0; ligne <= ligne; ligne++) {
+        let colonne = 0
+        if (led.point(colonne, ligne)) {
+            let dir: number[] = []
+            pro_lingne = ligne + dir[colonne]
+            if (pro_lingne >= 4) {
+                dir[colonne] = -1
+            } else if (pro_lingne <= 0) {
+                dir[colonne] = 1
+            }
+            led.unplot(colonne, ligne)
+            led.plot(colonne, pro_lingne)
+            break;
+        }
+    }
+}
+let pro_lingne = 0
+let liste = [1, 1, 1, 1, 1]
+led.plot(0, 0)
+led.plot(1, 1)
+led.plot(2, 2)
+led.plot(3, 3)
+led.plot(4, 4)
 basic.forever(function () {
-    basic.pause(100)
-    led.unplot(x, y)
-    y += 1
-    led.plotBrightness(x, y, 255)
-    led.unplot(x, y1)
-    y1 += 1
-    led.plotBrightness(x, y1, 160)
-    led.unplot(x, y2)
-    y2 += 1
-    led.plotBrightness(x, y2, 49)
-    if (y > 4) {
-        led.unplot(x, y)
-        y = -1
-    }
-    if (y1 > 4) {
-        led.unplot(x, y1)
-        y1 = -1
-    }
-    if (y2 > 4) {
-        led.unplot(x, y2)
-        y2 = -1
+    for (let colonne_b = 0; colonne_b <= 4; colonne_b++) {
+        deplace_led(colonne_b)
     }
 })
